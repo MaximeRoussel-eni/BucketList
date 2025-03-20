0babce89c8c14e847eff1b7cc67eeece2acdfc94
MaximeRoussel-eni <maxime.roussel1999@gmail.com>
2025-03-20 16:22:40 +0100

    Création du controller main et de son template twig

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use list\src\Repository\WishRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    /*#[Route('/admin', name: 'app_admin_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }*/
+    #[Route('/admin', name: 'app_admin_dashboard')]
+    public function dashboard(WishRepository $wishRepository): Response
+    {
+
+        $wishes = $wishRepository->findAll();
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+            'wishes' => $wishes
+        ]);
+    }
+
+}
---
+++ src/Repository/WishRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace bucket-list\src\Repository;
+
+use list\src\Entity\Wish;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<list\src\Entity\Wish>
+ */
+class WishRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Wish::class);
+    }
+
+    //    /**
+    //     * @return Wish[] Returns an array of Wish objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('w')
+    //            ->andWhere('w.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('w.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Wish
+    //    {
+    //        return $this->createQueryBuilder('w')
+    //            ->andWhere('w.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
